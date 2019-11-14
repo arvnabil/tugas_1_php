38045d4bd0f70a56dec54ca7ca795062896f18a7
Nabil <nabilalcha58@gmail.com>
2019-11-14 14:36:03 +0700

    Tugas 1 PHP
    Tugas ini kami kumpulkan untuk Niomic

---
+++ Tugas1.php
@@ -0,0 +1,36 @@
+<?php
+$namaMenu = "Ayam Goreng 	= ";
+$namaMenu1 = "Ayam Bakar 	= ";
+$namaMenu2 = "Ayam Sayur 	= ";
+$namaMenu3 = "Tempe Goreng  = ";
+$namaMenu4 = "Tahu Goreng 	= ";
+$namaMenu5 = "Soto 			= ";
+$namaMenu6 = "Nasi Putih 	= ";
+$namaMenu7 = "Es Teh 		= ";
+$namaMenu8 = "Es Tebu 		= ";
+
+$harga = "Rp 13000";
+$harga1 = "Rp 15000";
+$harga2 = "Rp 13000";
+$harga3 = "Rp 1000";
+$harga4 = "Rp 1000";
+$harga5 = "Rp 3000";
+$harga6 = "Rp 5000";
+$harga7 = "Rp 4000";
+$harga8 = "Rp 5000";
+
+
+echo "<h1> Daftar Menu</h1> <br>";
+echo "<ul>
+<li>$namaMenu $harga</li>
+<li>$namaMenu1 $harga1</li>
+<li>$namaMenu2 $harga2</li>
+<li>$namaMenu3 $harga3</li>
+<li>$namaMenu4 $harga4</li>
+<li>$namaMenu5 $harga5</li>
+<li>$namaMenu6 $harga6</li>
+<li>$namaMenu7 $harga7</li>
+<li>$namaMenu8 $harga8</li>
+</ul>";
+
+?>
---
+++ Tugas1_2.php
@@ -0,0 +1,53 @@
+<?php
+$namaMenu = "Cireng	";
+$namaMenu1 = "Es Duren 	";
+$namaMenu2 = "Es Milo 	";
+$namaMenu3 = "Es Teh  ";
+$namaMenu4 = "Piscok 	";
+
+$harga = "Rp 1000";
+$harga1 = "Rp 15000";
+$harga2 = "Rp 20000";
+$harga3 = "Rp 5000";
+$harga4 = "Rp 1000";
+
+echo "<h1>DAFTAR MENU</h1> <br>";
+echo "<table border='1'>
+		<thead>
+			<tr>
+				<td>No</td>
+				<td>Nama Menu</td>
+				<td>Harga</td>
+			</tr>
+		</thead>
+		<tbody>	
+			<tr>
+				<td>1</td>
+				<td>$namaMenu</td>
+				<td>$harga</td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>$namaMenu1</td>
+				<td>$harga1</td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>$namaMenu2</td>
+				<td>$harga2</td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>$namaMenu3</td>
+				<td>$harga3</td>
+			</tr>
+			<tr>
+				<td>5</td>
+				<td>$namaMenu4</td>
+				<td>$harga4</td>
+			</tr>			
+		</tbody>
+	</table>
+	"
+
+?>
